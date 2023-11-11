abea6c139315a9579b0f0eb22031b11168d0abe0
i-xi-dev <naoyuki.isono@gmail.com>
2023-11-11 22:41:59 +0900

    1.0.20

--- deps.ts
+++ deps.ts
@@ -1,1 +1,1 @@
-export { _TransformStream } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.2.7/mod.ts";
+export { _TransformStream } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.2.8/mod.ts";
